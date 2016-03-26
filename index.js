86390709214a9d790da063fcbb1d0d1e748b316e
cfullerton <conner0987@hotmail.com>
2016-03-25 20:35:38 -0500

    prod add module added

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("connected");
-});
+
 app.set('views', './views');
 app.set('view engine', 'jade');
 app.get('/product/:tagId', function (req, res) {
   res.render('prod', { title: 'Hey', message: req.params.tagId});
+  var newProduct = require('new-product');
+  newProduct({name:req.params.tagId});
 });
 server.listen(8081);   
