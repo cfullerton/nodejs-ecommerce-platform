31d7ca5706fed92d51d96df28ac1cc1f8a4e2291
cfullerton <conner0987@hotmail.com>
2016-03-26 03:11:43 -0500

    added product getting

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var app = require('express')();
 var server = require('http').createServer(app);
-
+var mongoose = require('mongoose');
+var productSchema = mongoose.Schema({
+    code: String,
+    fields: {}
+});
+var Product = mongoose.model('Product', productSchema);
 app.set('views', './views');
 app.set('view engine', 'jade');
-app.get('/product/:tagId', function (req, res) {
-  res.render('prod', { title: 'Hey', message: req.params.tagId});
-  var newProduct = require('new-product');
-  newProduct({name:req.params.tagId});
+var newProduct = require('new-product');
+newProduct({code:"testProd4",name:"test product",price:"1.00"},Product);
+app.get('/product/:productCode', function(req, res) {
+    var getProduct = require("get-product");
+	var productInfo = getProduct('testProd4',Product);
+	console.log(productInfo);
+    res.render('prod', {
+            title: 'Hey',
+            message: "Name: " + productInfo.fields.name
+    });
 });
-server.listen(8081);   
+server.listen(8081);
+
+
